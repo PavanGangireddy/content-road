00b6e836678f5fa107c1979d05d8b27a1dcb4177
PavanGangireddy <pavangangireddy@ibhubs.co>
2022-04-27 21:15:01 +0530

    added prisma next config data

--- apps/practice-prisma/lib/apollo.ts
+++ apps/practice-prisma/lib/apollo.ts
@@ -1,9 +1,16 @@
-// /lib/apollo.ts
+// lib/apollo.ts
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { relayStylePagination } from '@apollo/client/utilities';
 
-const apolloClient = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
-  cache: new InMemoryCache()
+export const client = new ApolloClient({
+  uri: '/api/graphql',
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          links: relayStylePagination()
+        }
+      }
+    }
+  })
 });
-
-export { apolloClient };
